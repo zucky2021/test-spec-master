4fafc4032276240162f9afc7116940bb6192ef5f
kanbaru-github <usupfv@gmail.com>
2024-07-10 00:26:12 +0900

    Create infrastructure layer

---
+++ app/Infrastructure/Repositories/ProjectRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Infrastructure\Repositories;
+
+use App\Domain\Project\ProjectFactory;
+use App\Domain\Project\ProjectRepositoryInterface;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * Project DB repository
+ */
+final class ProjectRepository implements ProjectRepositoryInterface
+{
+    public function findAll(): array
+    {
+        $projects = DB::table(self::TABLE_NAME)
+            ->whereNull('deleted_at')
+            ->get()
+            ->map(function ($project) {
+                return ProjectFactory::create((array) $project);
+            });
+
+        return $projects->toArray();
+    }
+}
