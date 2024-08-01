57b4c50d8668453382ff338a94ffd16e7669ac92
kanbaru-github <usupfv@gmail.com>
2024-08-02 00:06:09 +0900

    Create spec doc sheet seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             SpecificationDocumentSeeder::class,
             ExecutionEnvironmentSeeder::class,
+            SpecDocSheetSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/SpecDocSheetSeeder.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Domain\SpecDocSheet\SpecDocSheetDto;
+use App\Domain\SpecDocSheet\SpecDocSheetFactory;
+use App\Domain\SpecDocSheet\SpecDocSheetRepositoryInterface;
+use App\Domain\SpecDocSheet\ValueObject\StatusId;
+use DateTimeImmutable;
+use Exception;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class SpecDocSheetSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $values = [
+            [
+                'spec_doc_id' => 1,
+                'exec_env_id' => 1,
+            ],
+        ];
+
+        $insertData = [];
+        foreach ($values as $val) {
+            $dto = new SpecDocSheetDto(
+                id: null,
+                specDocId: $val['spec_doc_id'],
+                execEnvId: $val['exec_env_id'],
+                statusId: array_key_first(StatusId::STATUSES),
+                updatedAt: new DateTimeImmutable(),
+            );
+            $entity       = SpecDocSheetFactory::create($dto);
+            $insertData[] = [
+                'spec_doc_id' => $entity->getSpecDocId(),
+                'exec_env_id' => $entity->getExecEnvId(),
+                'status_id'   => $entity->getStatusId()->value(),
+                'created_at'  => now(),
+                'updated_at'  => now(),
+            ];
+        }
+
+        try {
+            DB::table(SpecDocSheetRepositoryInterface::TABLE_NAME)->insert($insertData);
+        } catch (Exception $e) {
+            Log::error('Failed to insert: ' . $e->getMessage());
+        }
+    }
+}
