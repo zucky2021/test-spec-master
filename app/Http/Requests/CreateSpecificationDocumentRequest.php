7bc92d1f9e7c53d512e985def368bdcd3bd57a68
kanbaru-github <usupfv@gmail.com>
2024-08-13 12:15:00 +0900

    Create create spec doc request

---
+++ app/Http/Requests/CreateSpecificationDocumentRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Domain\SpecificationDocument\ValueObject\Summary;
+use App\Domain\SpecificationDocument\ValueObject\Title;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateSpecificationDocumentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title'   => 'required|string|max:' . Title::MAX_LEN,
+            'summary' => 'required|string|max:' . Summary::MAX_LEN,
+        ];
+    }
+}
