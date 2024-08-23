c7df6a22a9d4c607d85bcebe11d4b009b3a405e1
kanbaru-github <usupfv@gmail.com>
2024-08-23 09:49:32 +0900

    Add back link

--- resources/ts/Pages/SpecDocSheet/Index.tsx
+++ resources/ts/Pages/SpecDocSheet/Index.tsx
@@ -1,6 +1,6 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import React from "react";
-import ReactMarkdown from 'react-markdown';
+import ReactMarkdown from "react-markdown";
 import { PageProps } from "@/types";
 import { Head, Link } from "@inertiajs/react";
 import { SpecDocSheet } from "@/types/SpecDocSheet";
@@ -25,23 +25,37 @@ const Index: React.FC<Props> = ({ auth, specDoc, specDocSheets }) => {
             <Head title="Specification document sheet list" />
 
             <section className="spec-doc-sheet">
+                <Link
+                    href={route("specDocs.index", {
+                        projectId: specDoc.projectId,
+                    })}
+                >
+                    Back to specification document list
+                </Link>
+
                 <div className="spec-doc-sheet__head">
                     <h3>{specDoc.title}</h3>
                     <ReactMarkdown>{specDoc.summary}</ReactMarkdown>
                 </div>
-                { auth.user.id === specDoc.userId && (
-                    <Link href={route("specDocs.edit", { projectId: specDoc.projectId, specDocId: specDoc.id })}>
+                {auth.user.id === specDoc.userId && (
+                    <Link
+                        href={route("specDocs.edit", {
+                            projectId: specDoc.projectId,
+                            specDocId: specDoc.id,
+                        })}
+                    >
                         Edit
                     </Link>
                 )}
-
             </section>
 
             <ul>
                 {specDocSheets.length > 0 ? (
                     specDocSheets.map((specDocSheet) => (
                         <li key={specDocSheet.id}>
-                            <Link href={`/project/${specDocSheet.specDocId}/spec-doc/${specDocSheet.id}`}>
+                            <Link
+                                href={`/project/${specDocSheet.specDocId}/spec-doc/${specDocSheet.id}`}
+                            >
                                 <h3>{specDocSheet.execEnvName}</h3>
                             </Link>
                         </li>
