014a5209873b16defafefd930548380207ed9134
zucky2021 <usupfv@gmail.com>
2025-02-19 16:10:23 +0900

    23 markup

--- resources/ts/Pages/SpecDocSheet/Show.tsx
+++ resources/ts/Pages/SpecDocSheet/Show.tsx
@@ -1,11 +1,10 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import React, { useState } from "react";
+import { ReactElement, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { PageProps } from "@/types";
 import { Head, Link } from "@inertiajs/react";
 import { SpecDocSheet } from "@/types/SpecDocSheet";
-import "@scss/pages/spec_doc_item/show.scss";
 import { SpecificationDocument } from "@/types/SpecificationDocument";
 import { SpecDocItem } from "@/types/SpecDocItem";
 import axios from "axios";
@@ -25,14 +24,14 @@ type ToggleStatusResponse = {
   newStatusId: number;
 };
 
-const Show: React.FC<Props> = ({
+const Show = ({
   auth,
   specDoc,
   specDocSheet,
   specDocItems,
   statuses,
   breadcrumbs,
-}) => {
+}: Props): ReactElement => {
   const [items, setItems] = useState<SpecDocItem[]>(specDocItems);
   const [loading, setLoading] = useState<number | null>(null);
 
@@ -64,37 +63,44 @@ const Show: React.FC<Props> = ({
     <AuthenticatedLayout
       user={auth.user}
       header={
-        <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+        <h1 className="font-semibold text-xl text-gray-800 leading-tight">
           Execute test
-        </h2>
+        </h1>
       }
     >
       <Head title="Execute test" />
 
       <Breadcrumbs breadcrumbs={breadcrumbs} />
 
-      <section className="spec-doc-exec">
+      <section className="mx-auto max-w-screen-lg">
         <Link
           href={route("specDocSheets.index", {
             projectId: specDoc.projectId,
             specDocId: specDoc.id,
           })}
-          className="back-link"
+          className="font-serif bg-gray-500 text-white rounded-full w-20 block text-center p-2 my-5 hover:opacity-50"
         >
-          Back to sheet list page
+          Back
         </Link>
 
-        <div className="spec-doc-item-edit__description">
-          <h3>{specDoc.title}</h3>
-          <h4>{specDocSheet.execEnvName}</h4>
-          <details>
-            <summary>Summary</summary>
+        <div className="shadow-md rounded-md p-4 ">
+          <h2 className="text-2xl font-bold">{specDoc.title}</h2>
+          <h3 className="text-xl font-serif my-2">
+            {specDocSheet.execEnvName}
+          </h3>
+          <details className="shadow-md p-2 rounded-md text-lg">
+            <summary className="cursor-pointer hover:opacity-50">
+              Summary
+            </summary>
             <ReactMarkdown remarkPlugins={[remarkGfm]} className="markdown">
               {specDoc.summary}
             </ReactMarkdown>
           </details>
-          <p>
-            Updated at: <time>{specDocSheet.updatedAt}</time>
+          <p className="shadow-md max-w-fit p-2 mx-auto mt-2 rounded-full text-md font-serif">
+            Updated at:
+            <time className="text-green-500 ml-2 text-lg">
+              {specDocSheet.updatedAt}
+            </time>
           </p>
         </div>
 
@@ -103,48 +109,59 @@ const Show: React.FC<Props> = ({
           specDoc={specDoc}
           specDocSheetId={specDocSheet.id}
         />
+      </section>
 
-        <ul className="spec-doc-item-edit__inputList">
-          <li>
-            <div>No.</div>
-            <div>Target area</div>
-            <div>Check detail</div>
-            <div>Remark</div>
-            <div></div>
-          </li>
+      <table className="border-black border-2 w-fit max-w-screen-xl overflow-scroll mx-auto">
+        <thead className="bg-gray-400">
+          <tr>
+            <th className="w-10 text-center border-black border">No.</th>
+            <th className="w-80 text-center border-black border">
+              Target area
+            </th>
+            <th className="w-80 text-center border-black border">
+              Check detail
+            </th>
+            <th className="w-48 text-center border-black border">Remark</th>
+            <th className="w-32 text-center border-black border">Status</th>
+          </tr>
+        </thead>
+        <tbody>
           {items.map((item, index) => (
-            <li key={index}>
-              <div>{index + 1}</div>
-              <div>
-                <ReactMarkdown remarkPlugins={[remarkGfm]}>
+            <tr key={index}>
+              <td className="border-black border text-center font-bold font-serif">
+                {index + 1}
+              </td>
+              <td className="border-black border p-1">
+                <ReactMarkdown remarkPlugins={[remarkGfm]} className="markdown">
                   {item.targetArea}
                 </ReactMarkdown>
-              </div>
-              <div>
-                <ReactMarkdown remarkPlugins={[remarkGfm]}>
+              </td>
+              <td className="border-black border p-1">
+                <ReactMarkdown remarkPlugins={[remarkGfm]} className="markdown">
                   {item.checkDetail}
                 </ReactMarkdown>
-              </div>
-              <div>
-                <ReactMarkdown remarkPlugins={[remarkGfm]}>
+              </td>
+              <td className="border-black border p-1">
+                <ReactMarkdown remarkPlugins={[remarkGfm]} className="markdown">
                   {item.remark}
                 </ReactMarkdown>
-              </div>
-              <div>
+              </td>
+              <td className="border-black border">
                 <button
                   type="button"
                   onClick={() => toggleStatus(index, item.id)}
                   disabled={loading === item.id}
+                  className="bg-blue-600 text-white p-1 mx-auto block rounded-md hover:opacity-50"
                 >
                   {loading === item.id
                     ? "Updating..."
                     : statuses[item.statusId]}
                 </button>
-              </div>
-            </li>
+              </td>
+            </tr>
           ))}
-        </ul>
-      </section>
+        </tbody>
+      </table>
     </AuthenticatedLayout>
   );
 };
